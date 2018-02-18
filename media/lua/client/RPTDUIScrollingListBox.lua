2724dcf0ba89db28053c80a1a62b713522e7a6f5
Rob Goodberry <robertgoodberry@robertgoodberry.com>
2018-02-18 04:16:08 -0500

    Fix indentation, inventory xfer and fill landfill

--- media/lua/client/RPTDUIScrollingListBox.lua
+++ media/lua/client/RPTDUIScrollingListBox.lua
@@ -1,20 +1,20 @@
 RPTDUIScrollingListBox = ISScrollingListBox:derive("RPTDUIScrollingListBox");
 
 function RPTDUIScrollingListBox:onMouseDoubleClick(x, y)
-	if self.onmousedblclick and self.items[self.selected] ~= nil then
-		local removedItem = self:removeItemByIndex(self.selected);
-		
-		self.onmousedblclick(removedItem, self.target1, self.target2, self.target3);
-		
-		local newModData = self:createModDataFromItems();
-		local existingModData = getSpecificPlayer(self.player):getModData();
+    if self.onmousedblclick and self.items[self.selected] ~= nil then
+        local removedItem = self:removeItemByIndex(self.selected);
+        
+        self.onmousedblclick(removedItem, self.target1, self.target2, self.target3);
+        
+        local newModData = self:createModDataFromItems();
+        local existingModData = getSpecificPlayer(self.player):getModData();
 
-		existingModData.rptdtags = newModData;
-	end
+        existingModData.rptdtags = newModData;
+    end
 end
 
 function RPTDUIScrollingListBox:renderItemsFromModData(modData)
-	self:clear();
+    self:clear();
     if modData then
         for tag, _ in pairs(modData) do
             local itemQuantityTable = {};
@@ -35,52 +35,52 @@ function RPTDUIScrollingListBox:sort()
 end
 
 function RPTDUIScrollingListBox:createModDataFromItems()
-	local newModData = {};
+    local newModData = {};
 
-	for i = 1, #self.items do
-		local item = self.items[i];
+    for i = 1, #self.items do
+        local item = self.items[i];
 
-		newModData[item.text] = item.text;
-	end
+        newModData[item.text] = item.text;
+    end
 
-	return newModData;
+    return newModData;
 end
 
 function RPTDUIScrollingListBox:setOnMouseDoubleClick(onmousedblclick, target1, target2, target3)
-	self.onmousedblclick = onmousedblclick;
-	self.target1 = target1;
-	self.target2 = target2;
-	self.target3 = target3;
+    self.onmousedblclick = onmousedblclick;
+    self.target1 = target1;
+    self.target2 = target2;
+    self.target3 = target3;
 end
 
 function RPTDUIScrollingListBox:new (x, y, width, height, player)
-	local o = {}
-	o = ISPanelJoypad:new(x, y, width, height);
-	setmetatable(o, self)
-	self.__index = self
-	o.x = x;
-	o.y = y;
-	o:noBackground();
-	o.backgroundColor = {r=0, g=0, b=0, a=0.8};
-	o.borderColor = {r=0.4, g=0.4, b=0.4, a=0.9};
-	o.altBgColor = {r=0.2, g=0.3, b=0.2, a=0.1}
-	o.altBgColor = nil
-	o.drawBorder = false
-	o.width = width;
-	o.height = height;
-	o.anchorLeft = true;
-	o.anchorRight = false;
-	o.anchorTop = true;
-	o.anchorBottom = false;
-	o.font = UIFont.Large
-	o.fontHgt = getTextManager():getFontFromEnum(o.font):getLineHeight()
-	o.itemPadY = 7
-	o.itemheight = o.fontHgt + o.itemPadY * 2;
-	o.selected = 1;
+    local o = {}
+    o = ISPanelJoypad:new(x, y, width, height);
+    setmetatable(o, self)
+    self.__index = self
+    o.x = x;
+    o.y = y;
+    o:noBackground();
+    o.backgroundColor = {r=0, g=0, b=0, a=0.8};
+    o.borderColor = {r=0.4, g=0.4, b=0.4, a=0.9};
+    o.altBgColor = {r=0.2, g=0.3, b=0.2, a=0.1}
+    o.altBgColor = nil
+    o.drawBorder = false
+    o.width = width;
+    o.height = height;
+    o.anchorLeft = true;
+    o.anchorRight = false;
+    o.anchorTop = true;
+    o.anchorBottom = false;
+    o.font = UIFont.Large
+    o.fontHgt = getTextManager():getFontFromEnum(o.font):getLineHeight()
+    o.itemPadY = 7
+    o.itemheight = o.fontHgt + o.itemPadY * 2;
+    o.selected = 1;
     o.count = 0;
-	o.itemheightoverride = {}
-	o.items = {}
-	o.player = player;
-	o.columns = {};
-	return o
+    o.itemheightoverride = {}
+    o.items = {}
+    o.player = player;
+    o.columns = {};
+    return o
 end
